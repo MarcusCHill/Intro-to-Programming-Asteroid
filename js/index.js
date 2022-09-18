ac509dcaf1d7a9f2a7f3aafd64b78aa5f25f7691
Marcus Hill <marcus@Marcuss-MacBook-Air.local>
2022-09-17 20:29:19 -0400

    Update title, about, skills, and experience

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ Located where I want my list of skills to be by using desendant selector (#skill
 Using a for of loop I add each element of my skills array within a newly created list element
 Added each list element with array value into the skills list
 */
-let skills = ["HTML", "CSS", "JavaScript", "React.js", "Git", "Fetch API", ];
+let skills = ["React.js", "JavaScript", "Java", "HTML", "CSS", "Git", "Fetch API", "Figma", "GitHub", "Trello", "Scrum Methods",];
 let skillsList = document.querySelector("#skills ul");
 for (let skill of skills) {
   let newSkill = document.createElement("li");
@@ -310,7 +310,10 @@ const repositoryData = (data) => {
 fetch('https://api.github.com/users/MarcusCHill/repos')
   .then(res => checkStatus(res))
   .then(response => response.json())
-  .then(data => repositoryData(data))
+  .then(data =>
+    return (
+      console.log(data) repositoryData(data)
+    ))
   .catch(error => console.log("Looks like something went wrong with a request!", error))
 
 
